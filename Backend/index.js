f14579bd28c645e9cf9dd51267294057d766ee2e
JoydeepKrGhosh <joydeepghosh2022@gmail.com>
2024-09-01 12:33:03 +0530

    Chai is the frontend file, Testing

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Backend is running!'));
-app.listen(3001, () => console.log('Server started on port 3001'));
+app.listen(3000, () => console.log('Server started on port 5001'));
 
