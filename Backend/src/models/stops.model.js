bfb452c0ab38d6b8f24c138ab16a91caea5cdecd
MohammadBinAftab <48010758+MohammadBinAftab@users.noreply.github.com>
2024-09-10 22:58:59 +0530

    Update stops.model.js

--- Backend/src/models/stops.model.js
+++ Backend/src/models/stops.model.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');  
+const mongoose = require('mongoose');
 
-const stopSchema = new mongoose.Schema({  
-  stopId: { type: String, required: true, unique: true },  
-  name: { type: String, required: true }, 
-  location: {  
-    latitude: { type: Number, required: true },  
-    longitude: { type: Number, required: true },  
-  },  
-});  
+const stopSchema = new mongoose.Schema({
+  stopId: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  location: {
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true },
+  },
+});
 
-const Stop = mongoose.model('Stop', stopSchema);  
+const Stop = mongoose.model('Stop', stopSchema);
 
 module.exports = Stop;
